0a671cacf651c30d7979109a44eeabad2a8b6484
Evgenii Salnikov <17097402+eugenesalnikov@users.noreply.github.com>
2026-09-17 16:21:18 +0400

    small fix

--- database/seeders/DistrictSeeder.php
+++ database/seeders/DistrictSeeder.php
@@ -18,13 +18,19 @@ class DistrictSeeder extends Seeder
             ['name' => 'Центр', 'slug' => 'centr', 'sort' => 2],
             ['name' => 'Ближнее Засвияжье', 'slug' => 'blizhnee-zasviyazhe', 'sort' => 3],
             ['name' => 'Дальнее Засвияжье', 'slug' => 'dalnee-zasviyazhe', 'sort' => 4],
-            ['name' => '4-й микрорайон', 'slug' => '4-j-mikrorajon', 'sort' => 5],
+            ['name' => '4-й микрорайон', 'slug' => '4-j-mikrorayon', 'sort' => 5],
             ['name' => 'Киндяковка', 'slug' => 'kindyakovka', 'sort' => 6],
             ['name' => 'Нижняя Терраса', 'slug' => 'nizhnyaya-terrasa', 'sort' => 7],
             ['name' => 'Верхняя Терраса', 'slug' => 'verhnyaya-terrasa', 'sort' => 8],
-            ['name' => 'Новый город и Промзона', 'slug' => 'novyj-gorod-i-promzona', 'sort' => 9],
-            ['name' => 'Ишеевка', 'slug' => 'isheevka', 'sort' => 10],
-            ['name' => 'Лаишевка', 'slug' => 'laishevka', 'sort' => 11],
+            ['name' => 'Новый город', 'slug' => 'novyj-gorod', 'sort' => 9],
+            ['name' => 'Промзона', 'slug' => 'promzona', 'sort' => 10],
+            ['name' => 'Ишеевка', 'slug' => 'isheevka', 'sort' => 11],
+            ['name' => 'Лаишевка', 'slug' => 'laishevka', 'sort' => 12],
+            ['name' => 'Баратаевка', 'slug' => 'baratayevka', 'sort' => 13],
+            ['name' => 'Белый Ключ', 'slug' => 'belii-klyuch', 'sort' => 14],
+            ['name' => 'Сельдь', 'slug' => 'seld', 'sort' => 15],
+            ['name' => 'Пригородный', 'slug' => 'prigorodnii', 'sort' => 16],
+            ['name' => 'Луговое', 'slug' => 'lugovoye', 'sort' => 17],
         ];
 
         foreach ($districts as $district) {
